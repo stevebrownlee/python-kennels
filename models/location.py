6038573b6f7772a38574b64c79fe691ae41983c6
Steve Brownlee <chortlehoort@gmail.com>
2021-02-10 18:40:05 -0600

    Correct evaluation

--- models/location.py
+++ models/location.py
@@ -3,5 +3,5 @@ class Location():
     def __init__(self, name, address = ""):
         self.name = name
 
-        if address is not "":
+        if address != "":
             self.address = address
